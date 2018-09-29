c031beee8c2d348ce474ed5a33c4a5f5fd709d56
saragarmee <saragarmin@protonmail.com>
2018-09-28 22:21:43 -0600

    Simplified design a little

--- telepathic-loader.js
+++ telepathic-loader.js
@@ -3,32 +3,25 @@ import { importModule } from "https://uupaa.github.io/dynamic-import-polyfill/im
 export default class TelepathicLoader{
     static async Load(dom){
         let nodeList = dom.querySelectorAll('*');
-        nodeList.forEach(async (element)=>{
+        for(let element of nodeList){
             if(element.tagName.includes('-ELEMENT')){
                 let tagName = element.tagName.toLowerCase();
-                let jsFileName = `${tagName}.js`;
-                let htmlFileName = `${tagName}.html`;
-                let classParts = tagName.split('-');
-                let className = "";
-                classParts.forEach((part)=>{
-                    className += part.charAt(0).toUpperCase() + part.slice(1);
-                });
-    
                 //First see if there is already something in the custom element registry
                 if(!window.customElements.get(tagName)){
-                    //console.log(`${tagName} : ${className} is ${fileName}`);
-                    //console.log(`${tagName} is unregistered`);
-                    //Try to instantiate
+                    let jsFileName = `${tagName}.js`;
                     
+                    let classParts = tagName.split('-');
+                    let className = "";
+                    for(let part of classParts){
+                        className += part.charAt(0).toUpperCase() + part.slice(1);
+                    }
+                   
+                    window[tagName] = className;
                     let path = window.location.pathname.split('/');
                     path.pop();
                     path = path.join('/');
+                    window[className] = path;
                     let jsloc = `${path}/${tagName}/${jsFileName}`;
-                    let htmlLoc = `${path}/${tagName}/${htmlFileName}`;
-                    window[tagName] = className;
-                    window[className] = htmlLoc;
-                    console.log(`Expecting template at ${window[className]}`);
-                    
                     let module;
                     try{
                         console.log(`importing ${jsloc}`);
@@ -36,40 +29,17 @@ export default class TelepathicLoader{
                     }catch(err){
                         //module is probably remote
                         path = 'https://telepathic-elements.github.io';
-                        jsloc = `${path}/${tagName}/${jsFileName}`;
-                        let htmlLoc = `${path}/${tagName}/${htmlFileName}`;
-                        window[tagName] = className;
-                        window[className] = htmlLoc;
-                        console.log(`importing ${jsloc}`);
-                        module = await importModule(jsloc);
+                        jsLoc = `${path}/${tagName}/${jsFileName}`;
+                        console.log(`importing ${jsLoc}`);
+                        module = await importModule(jsLoc);
                     }
                     console.log(tagName+" module is ",module);
-                    TelepathicLoader.delayLoad(htmlLoc,tagName,className,module,1);
-
+                    await window.customElements.define(tagName,module.default);
                 }else{
                     console.log(`${tagName} already loaded, skipping!`);
                 }
             }
-        });
-    }
-
-    static delayLoad(htmlLoc,tagName,className,module,delay){
-        setTimeout(()=>{
-            if(!window.customElements.get(tagName)){
-                if(window[className]){
-                    try{
-                        window.customElements.define(tagName,module.default);
-                    }catch(err){
-                        console.error(`${tagName} : ${err}`);
-                        TelepathicLoader.delayLoad(htmlLoc,tagName,className,module,delay +1000);
-                    }
-                }else{
-                    window[className] = htmlLoc;
-                    console.error("Template location missing for ",className);
-                    TelepathicLoader.delayLoad(htmlLoc,tag,className,module,delay + 1000);
-                }
-            }
-        },delay);
+        }
     }
 }
 window.TelepathicLoader = TelepathicLoader;  
